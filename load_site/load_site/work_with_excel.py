70a41085555ab82712a220cc8269193f5627a582
Faserbito <147964334+Faserbito@users.noreply.github.com>
2023-11-12 12:52:05 +0500

    Delete load_site directory

--- load_site/load_site/work_with_excel.py
+++
@@ -1,9 +0,0 @@
-import openpyxl
-
-def work(name_sheet, dictionary):
-    workbook = openpyxl.load_workbook('rjd.xlsx')
-    sheet = workbook[name_sheet]
-    last_row = sheet.max_row
-    for i in dictionary.keys():
-        sheet.cell(row=last_row + 1, column=i, value=dictionary[i][1])
-    workbook.save('rjd.xlsx')
