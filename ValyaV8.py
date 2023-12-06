d8481d88089e968cbd546af4a028e5df442705d4
Faserbito <147964334+Faserbito@users.noreply.github.com>
2023-12-06 13:04:30 +0500

    Update ValyaV8.py

--- ValyaV8.py
+++ ValyaV8.py
@@ -41,7 +41,7 @@ def send_email(message):
     user = 'example_api@mail.ru'
     password = 'LkybB53PjruzpkPAjnFi'
     a = 1
-    if 1==1 & 2==2:
+    if 1==1 && 2==2:
         pass
     if a==a:
         a += 1
